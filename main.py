1c575916d75ca7cf46a7a1ae0e72b6184308dfbd
Бен Аладжем <benialadjem@gmail.com>
2022-07-30 17:52:55 +0300

    add config.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from flask_migrate import Migrate
 from db import db
 
 app = Flask(__name__)
-ap = Api(app)
+app.config.from_object("config.DevelopmentConfig")
+api = Api(app)
 migrate = Migrate(app, db)
+
+[api.add_resource(*route_data) for route_data in routes]
+
+db.init_app(app)
 
 if __name__ == "__main__":
     app.run()
