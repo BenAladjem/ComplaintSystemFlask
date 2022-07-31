5d26976668691b7a8ad449b38e2ba866d6fd7a0a
Бен Аладжем <benialadjem@gmail.com>
2022-07-31 12:24:31 +0300

    add config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class ProductionConfig:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = (
-        f"postgressql://{config('DB_USER')}:{config('DB_PASSWORD')}"
+        f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
         f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
     )
 
@@ -16,6 +16,6 @@ class DevelopmentConfig:
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = (
-        f"postgressql://{config('DB_USER')}:{config('DB_PASSWORD')}"
+        f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}"
         f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
     )
