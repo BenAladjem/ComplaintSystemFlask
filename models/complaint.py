9dfdca1b9004ed64b02172ff69f479dd2d680432
Бен Аладжем <benialadjem@gmail.com>
2022-07-31 13:29:19 +0300

    add migrations

--- models/complaint.py
+++ models/complaint.py
@@ -9,7 +9,7 @@ class Complaint(db.Model):
     photo_url = db.Column(db.String(255), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    amount = db.Column(db.float, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
     created_on = db.Column(db.DateTime, server_default=func.now())
     status = db.Column(db.Enum(ComplaintState), default=ComplaintState.pending, nullable=False)
     complainer_id = db.Column(db.Integer, db.ForeignKey('complainers.id'), nullable=False)
